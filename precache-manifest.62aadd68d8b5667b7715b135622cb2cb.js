a026d780615760976eae9f2935b4806bf03dfb89
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-25 22:33:38 +0000

    Deploy mrkosima/playground-hacker-news to github.com/mrkosima/playground-hacker-news.git:gh-pages

---
+++ precache-manifest.62aadd68d8b5667b7715b135622cb2cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b34dafb28c9f7fdf9c40",
+    "url": "/playground-hacker-news/static/css/main.55ded221.chunk.css"
+  },
+  {
+    "revision": "b34dafb28c9f7fdf9c40",
+    "url": "/playground-hacker-news/static/js/main.b34dafb2.chunk.js"
+  },
+  {
+    "revision": "eb759aa31a1f08f2c6e3",
+    "url": "/playground-hacker-news/static/js/1.eb759aa3.chunk.js"
+  },
+  {
+    "revision": "2f7e4737fc8604edb522",
+    "url": "/playground-hacker-news/static/js/runtime~main.2f7e4737.js"
+  },
+  {
+    "revision": "e516d433546e71356553bf7d3b981416",
+    "url": "/playground-hacker-news/index.html"
+  }
+];
